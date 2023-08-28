aeb6b235fada87b0fccd6964ec6c82726d740dfd
wall randal <wallace.moura@aiesec.net>
2023-08-28 10:56:46 -0300

    removendo reinicializacao incorreta do git

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:wallrandal/meu-portfolio.git main:gh_pages
+git push -f git@github.com:wallrandal/meu_portfolio.git main:gh_pages
 
 cd -
